60837e3c325c7b4920b11a569eaf10a1f8fb8954
Charles-OpsWerks <charles.perez@academy.opswerks.com>
2024-09-04 21:57:10 +0800

    Added subscript function

--- subscripts/save_user.sh
+++ subscripts/save_user.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
 echo "[+] Saving user data..."
+
+save_user() {
+    echo "$name, $shift, $time, $team" >> ./data/team_shift.txt
+    echo "[+] User data saved successfully."
+}
+
+save_user
--- subscripts/validate_capacity.sh
+++ subscripts/validate_capacity.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+echo "[+] Validating capacity..."
 
-echo "[+] Validating Capacity..."
+validate_capacity() {
+    
+    shift_count=$(grep "$shift" ./data/team_shift.txt | grep -c "$team")
+    
+    if [[ $shift_count -ge 2 ]]; then
+        echo "[!] Error: Shift $shift is already full for team $team. Exiting"
+        exit 1
+    fi
+
+}
+
+validate_capacity
