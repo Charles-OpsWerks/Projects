fdeab5d7e39b8dc4406e7844b6b31870ced673d8
MrAgui <karlaguilar@gmail.com>
2024-09-04 23:34:49 +0800

    Add color.sh

---
+++ subscripts/colors.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export NEON_GREEN='\033[1;92m'
+export NEON_CYAN='\033[1;96m'
+export NEON_MAGENTA='\033[1;95m'
+export NEON_YELLOW='\033[1;93m'
+export NEON_RED='\033[1;91m'
+export NC='\033[0m' # No Color
